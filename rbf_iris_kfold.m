aa9eeb75634b4e60bbc28b3b16706420e427edba
Leila Rodrigues <39334895+leilamr@users.noreply.github.com>
2019-11-18 13:04:01 -0300

    Update rbf_iris_kfold.m

--- rbf_iris_kfold.m
+++ rbf_iris_kfold.m
@@ -100,8 +100,7 @@ for i=1:j
         trainMatrix = [k2;k3;k4;k5;k6;k7;k8;k9;k10];
         testMatrix = k1;
        end
-    
-       %% start MLP
+       
        %matrix for Train (135x4)
        inputTrain = trainMatrix(:,2:5);
        
